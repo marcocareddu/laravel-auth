d2a6c91cd271df0ca848281fab93d22011ff963f
marcocareddu <saldumaccu@tuta.io>
2023-09-05 17:42:27 +0200

    Fixed preview script

--- resources/js/preview.js
+++ resources/js/preview.js
@@ -1,5 +1,5 @@
 // Active thumbnail
-const placeholder = 'https://t3.ftcdn.net/jpg/03/45/05/92/360_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg';
+const placeholder = 'https://marcolanci.it/utils/placeholder.jpg';
 const thumbnail = document.getElementById('thumb');
 const thumbnailPreview = document.getElementById('thumbnail-preview');
 
@@ -8,7 +8,7 @@ let tempUrl = null;
 
 thumbnail.addEventListener('change', () => {
     if (thumbnail.files && thumbnail.files[0]) {
-        const file = thumbnail.file[0];
+        const file = thumbnail.files[0];
         tempUrl = URL.createObjectURL(file);
         thumbnailPreview.src = tempUrl;
     } else {
@@ -17,5 +17,5 @@ thumbnail.addEventListener('change', () => {
 });
 
 window.addEventListener('beforeunload', () => {
-    if (tempUrl) URL.createObjectURL(tempUrl);
+    if (tempUrl) URL.revokeObjectURL(tempUrl);
 })
