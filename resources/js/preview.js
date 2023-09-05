0c24725a7a85de705dbbce8f2a98775d5365d7dc
marcocareddu <saldumaccu@tuta.io>
2023-09-05 17:36:06 +0200

    Modified preview js

--- resources/js/preview.js
+++ resources/js/preview.js
@@ -2,6 +2,20 @@
 const placeholder = 'https://t3.ftcdn.net/jpg/03/45/05/92/360_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg';
 const thumbnail = document.getElementById('thumb');
 const thumbnailPreview = document.getElementById('thumbnail-preview');
-thumbnail.addEventListener('input', () => {
-    thumbnailPreview.src = thumbnail.value ? thumbnail.value : placeholder;
+
+// Temp var
+let tempUrl = null;
+
+thumbnail.addEventListener('change', () => {
+    if (thumbnail.files && thumbnail.files[0]) {
+        const file = thumbnail.file[0];
+        tempUrl = URL.createObjectURL(file);
+        thumbnailPreview.src = tempUrl;
+    } else {
+        thumbnailPreview.src = placeholder;
+    }
 });
+
+window.addEventListener('beforeunload', () => {
+    if (tempUrl) URL.createObjectURL(tempUrl);
+})
